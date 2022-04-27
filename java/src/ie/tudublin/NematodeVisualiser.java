2a0270253ec1da181afc534b88ebc85300883dd0
Olabode <C20478706@mytudublin.ie>
2022-04-27 18:43:32 +0100

    I unfortunately was unable to get the nematode to work correctly for some reason

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -2,17 +2,21 @@ package ie.tudublin;
 
 import java.util.ArrayList;
 
+import javax.swing.text.TableView;
+
 import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
 {
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
 
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
 		{
+			
 		}		
 	}
 
@@ -26,16 +30,41 @@ public class NematodeVisualiser extends PApplet
 	{
 		colorMode(HSB);
 		background(0);
-		smooth();				
+		smooth();		
+		loadNematodes();
+		printNematodes();		
 	}
 	
 
 	public void loadNematodes()
 	{
+		Table table = loadTable("nematodes.csv", "header");
+
+		for(TableRow r:table.rows())
+		{
+			Nematode n = new Nematode(r);
+			nematodes.add(n);
+		}
 	}
 
+	public void drawNematodes()
+	{
+		for(Nematode n:nematodes)
+		{
+			n.render(this);
+		}
+	}
+
+	public void printNematodes()
+	{
+		for(Nematode n:nematodes)
+		{
+			System.out.println(n);
+		}
+	}
 
 	public void draw()
 	{	
+		drawNematodes();
 	}
 }
